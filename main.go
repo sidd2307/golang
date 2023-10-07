fffd0fd517b662736323f27365f328a79af4b087
sidd2307 <siddheshwarcp.2307@gmail.com>
2023-10-07 23:18:50 +0530

    print statements

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = conferenceTickets
 
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Println("Welcome to", conferenceName, "booking application.")
 	fmt.Println("We have total of", conferenceTickets, "tickets &", remainingTickets, "are still left!!!")
 	fmt.Println("Get your tickets here to attend.")
